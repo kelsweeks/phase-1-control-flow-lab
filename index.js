ddb6a9bd2e69d169d4d307a162be42b1f3a68c15
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-06-08 21:47:46 -0500

    commit tree f0d8d1bc3a2727e8318e4536d6f225bb763c8b81

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// give customers a sample is the ride is less than or equal to 400 feet
+// charges 20 dollars for a distance between 400 and 2000 feet
+// charges 30 dollars for a distance over 2000 feet
+// does not allow rides over 2500 feet
+function scuberGreetingForFeet(someValue) {
+ if (someValue <= 400) {
+   return 'This one is on me!'
+}
+else if (someValue > 400 && someValue <= 2000) {
+  return 'That will be twenty bucks.'
+}
+else if (someValue > 2000 && someValue <= 2500) {
+  return 'I will gladly take your thirty bucks.'
+}
+else if (someValue > 2500) {
+  return 'No can do.'
+}
+}
+// returns "Ok, sounds good." when the city is NYC
+// should return "No go." if the destination city is not NYC
+function ternaryCheckCity(someCity) {
+   let result = someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+    return result 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+// should return "Thank you so much." if the tip is generous
+// should return "Thank you." if the tip is not as generous
+// should return "Bye." if anything else
+// how do we know what is generous? 
+function switchOnCharmFromTip(tip) {
+  let result
+    switch(tip) {
+     case 'generous' :
+       return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default: return 'Bye.'
+    }
 }
